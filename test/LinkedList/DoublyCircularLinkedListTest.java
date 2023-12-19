3fe453c7b89a77ee4ad1be952430d73435347f85
houyaohui <u7754892@anu.edu.au>
2023-12-20 00:25:18 +1100

    updated DoublyCircularLinkedList

---
+++ test/LinkedList/DoublyCircularLinkedListTest.java
@@ -0,0 +1,109 @@
+package LinkedList;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoublyCircularLinkedListTest {
+    private DoublyCircularLinkedList getList(){
+        DoublyCircularLinkedList list = new DoublyCircularLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+        return list;
+    }
+
+
+    @Test
+    void addFirstTest() {
+        DoublyCircularLinkedList list = new DoublyCircularLinkedList();
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addFirst(4);
+        list.addFirst(5);
+
+        assertIterableEquals(List.of(5, 4, 3, 2, 1), list);
+    }
+
+    @Test
+    void addLastTest() {
+        DoublyCircularLinkedList list = new DoublyCircularLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+
+        assertIterableEquals(List.of(1, 2, 3, 4, 5), list);
+    }
+
+
+    @Test
+    void removeFirstTest() {
+        DoublyCircularLinkedList list = getList();
+        list.removeFirst();
+        assertIterableEquals(List.of(2, 3, 4, 5), list);
+
+        list.removeFirst();
+        assertIterableEquals(List.of(3, 4, 5), list);
+
+        list.removeFirst();
+        assertIterableEquals(List.of(4, 5), list);
+
+        list.removeFirst();
+        assertIterableEquals(List.of(5), list);
+
+        list.removeFirst();
+        assertIterableEquals(List.of(), list);
+
+        assertThrows(IllegalArgumentException.class, list::removeFirst);
+    }
+
+    @Test
+    void removeLastTest() {
+        DoublyCircularLinkedList list = getList();
+        list.removeLast();
+        assertIterableEquals(List.of(1, 2, 3, 4), list);
+
+        list.removeLast();
+        assertIterableEquals(List.of(1, 2, 3), list);
+
+        list.removeLast();
+        assertIterableEquals(List.of(1, 2), list);
+
+        list.removeLast();
+        assertIterableEquals(List.of(1), list);
+
+        list.removeLast();
+        assertIterableEquals(List.of(), list);
+
+        assertThrows(IllegalArgumentException.class, list::removeLast);
+    }
+
+    @Test
+    void removeByValueTest() {
+        DoublyCircularLinkedList list = getList();
+        list.removeByValue(1);
+        assertIterableEquals(List.of(2, 3, 4, 5), list);
+
+        list.removeByValue(3);
+        assertIterableEquals(List.of(2, 4, 5), list);
+
+        list.removeByValue(5);
+        assertIterableEquals(List.of(2, 4), list);
+
+        list.removeByValue(2);
+        assertIterableEquals(List.of(4), list);
+
+        list.removeByValue(8);
+        assertIterableEquals(List.of(4), list);
+
+        list.removeByValue(4);
+        assertIterableEquals(List.of(), list);
+    }
+}
